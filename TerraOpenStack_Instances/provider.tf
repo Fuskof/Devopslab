44bafb577fa2bf63dfe248e3612e88a6a7e4cf04
Fuskof <techclub89@gmail.com>
2020-07-22 14:23:52 +0300

    deleted old Terrafor folder

--- TerraOpenStack_Instances/provider.tf
+++
@@ -1,7 +0,0 @@
-# Configure the OpenStack Provider
-provider "openstack" {
-  user_name   = var.openstack_user_auth
-  tenant_name = var.openstack_tenant_auth
-  password    = var.openstack_user_pass
-  auth_url    = var.openstack_auth_url
-}
